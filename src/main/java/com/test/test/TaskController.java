454c915009096a7f7cae654e3044b50d6c53345a
Bohdan Kalitovskyi <12322ss87@gmail.com>
2026-05-06 18:13:25 +0200

    add post,put,patch,delete requests

--- src/main/java/com/test/test/TaskController.java
+++ src/main/java/com/test/test/TaskController.java
@@ -1,35 +1,85 @@
 package com.test.test;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
 
+    private static final Logger log = LoggerFactory.getLogger(TaskController.class);
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
     @GetMapping("/{id}")
-    public Task getTaskById(
+    public ResponseEntity<Task> getTaskById(
            @PathVariable("id") Long id
     ){
-        System.out.println("by id");
-        return taskService.getTaskById(id);
+        log.info("called getTaskById");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(taskService.getTaskById(id));
     }
 
     @GetMapping
-    public List<Task> getAllTasks(
+    public ResponseEntity<List<Task>> getAllTasks(
     ){
-        System.out.println("all");
-        return taskService.findAllTasks();
+        log.info("called getAllById");
+        return ResponseEntity.ok(taskService.findAllTasks());
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(
+            @RequestBody Task taskToCreate
+    ){
+        log.info("called createTask");
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("test-header","123")
+                .body(taskService.createTask(taskToCreate));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(
+            @PathVariable("id") Long id,
+            @RequestBody Task updatedTask
+    ){
+        log.info("called updateTask");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(taskService.updateTask(id,updatedTask));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(
+            @PathVariable("id") Long id
+    ){
+        log.info("called deleteTask");
+
+        try {
+            taskService.deleteTask(id);
+            return ResponseEntity.ok()
+                    .build();
+        }
+        catch (NoSuchElementException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    @PatchMapping("/{id}/undo")
+    public ResponseEntity<Task> undoTask(
+            @PathVariable("id") Long id
+    ){
+        log.info("called undoTask");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(taskService.undoTask(id));
     }
 }
--- src/main/java/com/test/test/TaskService.java
+++ src/main/java/com/test/test/TaskService.java
@@ -1,59 +1,122 @@
 package com.test.test;
 
+import org.apache.coyote.BadRequestException;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class TaskService {
 
 
 
-    private final Map<Long, Task> taskHashMap = Map.of(
-            1L, new Task(
-                    1L,
-                    10L,
-                    30L,
-                    TaskStatus.CREATED,
-                    LocalDateTime.now(),
-                    LocalDate.now().plusDays(2),
-                    TaskPriority.LOW
-            ),
-            2L, new Task(
-                    2L,
-                    13L,
-                    35L,
-                    TaskStatus.CREATED,
-                    LocalDateTime.now(),
-                    LocalDate.now().plusDays(3),
-                    TaskPriority.LOW
-            ),
-            3L, new Task(
-                    3L,
-                    17L,
-                    32L,
-                    TaskStatus.CREATED,
-                    LocalDateTime.now(),
-                    LocalDate.now().plusDays(1),
-                    TaskPriority.LOW
-            )
-    );
+    private final Map<Long, Task> taskMap;
+    private final AtomicLong idCounter;
+
+    public TaskService(Map<Long, Task> taskHashMap) {
+        taskMap = new HashMap<>();
+        idCounter = new AtomicLong();
+    }
 
 
     public Task getTaskById(
             Long id
     ) {
-        if(!taskHashMap.containsKey(id)){
+        if(!taskMap.containsKey(id)){
             throw new NoSuchElementException("Not found task by id = "+id);
         }
-        return taskHashMap.get(id);
+        return taskMap.get(id);
     }
 
     public List<Task> findAllTasks() {
-        return taskHashMap.values().stream().toList();
+        return taskMap.values().stream().toList();
+    }
+
+    public Task createTask(
+            Task taskToCreate
+    ) {
+        if(taskToCreate.id()!=null){
+            throw new IllegalArgumentException("id should be empty");
+        }
+
+        var newTask = new Task(
+                idCounter.incrementAndGet(),
+                taskToCreate.creatorId(),
+                taskToCreate.assignedUserId(),
+                TaskStatus.CREATED,
+                taskToCreate.createDateTime(),
+                taskToCreate.deadlineDate(),
+                taskToCreate.priority()
+        );
+
+        taskMap.put(newTask.id(),newTask);
+
+        return newTask;
+    }
+
+
+    public Task updateTask(Long id, Task updatedTask) {
+        if(!taskMap.containsKey(id)){
+            throw new NoSuchElementException("Not found task by id = "+id);
+        }
+        var task = taskMap.get(id);
+        if(task.status() == TaskStatus.DONE)
+        {
+            throw new IllegalStateException("Cannot update task which is already done");
+        }
+
+
+        var newUpdatedTask = new Task(
+                task.id(),
+                updatedTask.creatorId(),
+                updatedTask.assignedUserId(),
+                updatedTask.status(),
+                updatedTask.createDateTime(),
+                updatedTask.deadlineDate(),
+                updatedTask.priority()
+        );
+
+        taskMap.put(id, newUpdatedTask);
+
+        return newUpdatedTask;
+    }
+
+    public void deleteTask(Long id) {
+        if(!taskMap.containsKey(id)){
+            throw new NoSuchElementException("Not found task by id = "+id);
+        }
+        taskMap.remove(id);
+    }
+
+    public Task undoTask(Long id) {
+        if(!taskMap.containsKey(id)){
+            throw new NoSuchElementException("Not found task by id = "+id);
+        }
+        var taskToUndo = taskMap.get(id);
+        if(taskToUndo.status()!=TaskStatus.DONE)
+        {
+            throw new IllegalStateException("Cannot undo task that is not done yet");
+        }
+
+        var patchedTask =  new Task(
+                taskToUndo.id(),
+                taskToUndo.creatorId(),
+                taskToUndo.assignedUserId(),
+                TaskStatus.IN_PROGRESS,
+                taskToUndo.createDateTime(),
+                taskToUndo.deadlineDate(),
+                taskToUndo.priority()
+        );
+
+        taskMap.put(id, patchedTask);
+
+        return patchedTask;
     }
 }
